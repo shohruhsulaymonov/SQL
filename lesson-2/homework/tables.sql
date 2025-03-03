9565a573902f363243abc8f7413e69a2d3ba1ca2
Shohruh <shsulaymonov2007@gmail.com>
2025-03-03 10:56:37 +0500

    Update tables.sql

--- lesson-2/homework/tables.sql
+++ lesson-2/homework/tables.sql
@@ -1,3 +1,33 @@
+create table Salesman(salesman_id int, name varchar(25), city varchar(20), commission float)
+
+INSERT INTO Salesman VALUES
+(5001, 'James Hoog', 'New York', 0.15),
+(5002, 'Nail Knite', 'Paris', 0.13),
+(5005, 'Pit Alex', 'London', 0.11),
+(5006, 'Mc Lyon', 'Paris', 0.14),
+(5007, 'Paul Adam', 'Rome', 0.13),
+(5003, 'Lauson Hen', 'San Jose', 0.12);
+
+
+
+create table Orders (ord_no int, purch_amt float, ord_date date, customer_id int, salesman_id int);
+
+insert into Orders values
+(70001, 150.5, '2012-10-5', 3005, 5002)
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002),
+(70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006)
+(70003, 2480.4, '2012-10-10', 3009, 5003),
+(70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.6, '2012-04-25', 3002, 5001);
+
+
+
 create table Customer
 (customer_id int, cust_name varchar(25), city varchar(25), grade int, salesman_id int)
 
@@ -9,4 +39,70 @@ insert into Customer values
 (3004, 'Fabian Johnson', 'Paris', 300, 5006),
 (3009, 'Geoff Cameron', 'Berlin', 100, 5003),
 (3003, 'Jozi Altidor', 'Moscow', 200, 5007),
-(3001, 'Brad Guzan', 'London', null, 5005)
+(3001, 'Brad Guzan', 'London', null, 5005);
+
+
+
+CREATE TABLE nobel_win (year int, subject varchar(25), winner varchar(30),country varchar(20), category varchar(25))
+
+INSERT INTO nobel_win VALUES
+(1970, 'Physics', 'Hannes Alfven', 'Sweden', 'Scientist'),
+(1970, 'Physics', 'Louis Neel', 'France', 'Scientist'),
+(1970, 'Chemistry', 'Luis Federico Leloir', 'France', 'Scientist'),
+(1970, 'Physiology', 'Ulf von Euler', 'Sweden', 'Scientist'),
+(1970, 'Physiology', 'Bernard Katz', 'Germany', 'Scientist'),
+(1970, 'Literature', 'Aleksandr Solzhenitsyn', 'Russia', 'Linguist'),
+(1970, 'Economics', 'Paul Samuelson', 'USA', 'Economist'),
+(1970, 'Physiology', 'Julius Axelrod', 'USA', 'Scientist'),
+(1971, 'Physiology', 'Dennis Gabor', 'Hungary', 'Scientist'),
+(1971, 'Chemistry', 'Gerhard Herzberg', 'Germany', 'Scientist'),
+(1971, 'Peace', 'Willy Brandt', 'Germany', 'Chancellor'),
+(1971, 'Literature', 'Pablo Neruda', 'Chile', 'Linguist'),
+(1971, 'Economics', 'Simon Kuznets', 'Russia', 'Economist'),
+(1978, 'Peace', 'Anwar Al-Sadat', 'Egypt', 'President'),
+(1978, 'Peace', 'Menachem Begin', 'Israel', 'Prime Minister'),
+(1987, 'Chemistry', 'Donald J. Cram', 'USA', 'Scientist'),
+(1987, 'Chemistry', 'Jean-Marie Lehn', 'France', 'Scientist'),
+(1987, 'Physiology', 'Susumu Tonegawa', 'Japan', 'Scientist'),
+(1994, 'Economics', 'Reinhard Selten', 'Germany', 'Economist'),
+(1994, 'Peace', 'Yitzhak Rabin', 'Israel', 'Prime Minister'),
+(1997, 'Physics', 'Johannes Georg Bednorz', 'Germany', 'Scientist'),
+(1997, 'Literature', 'Joseph Brodsky', 'Russia', 'Linguist'),
+(1997, 'Economics', 'Robert Solow', 'USA', 'Economist'),
+(1994, 'Literature', 'Kenzaburo Oe', 'Japan', 'Linguist');
+
+
+
+create table item_mast (PRO_ID INT, PRO_NAME VARCHAR(25), PRO_PRICE DECIMAL(10, 2), PRO_COM INT)
+
+insert into item_mast values
+(101, 'Mother Board', '3200.00', 15),
+(102, 'Key Board', '450.00', 16),
+(103, 'ZIP Drive', '250.00', 14),
+(104, 'Speaker', '550.00', 16),
+(105, 'Monitor', '5000.00', 11),
+(106, 'DVD Drive', '900.00', 12),
+(107, 'CD Drive', '800.00', 12),
+(108, 'Printer', '2600.00', 13),
+(109, 'Refill Cartridge', '350.00', 13),
+(110, 'Mouse', '250.00', 12);
+
+
+
+
+create table emp_details (EMP_IDNO INT, EMP_FNAME VARCHAR(15), EMP_LNAME VARCHAR(15), EMP_DEP INT)
+
+INSERT INTO emp_details VALUES
+(127323, 'Michale', 'Robbin', 57),
+(526689, 'Carlos', 'Snares', 63),
+(843795, 'Enric', 'Dosio', 57),
+(328717, 'Jhon', 'Snares', 63),
+(444527, 'Joseph', 'Dosni', 47),
+(659831, 'Zanifer', 'Emily', 47),
+(847674, 'Kuleswar', 'Sitaraman', 57),
+(748681, 'Henrey', 'Gabriel', 47),
+(555935, 'Alex', 'Manuel', 57),
+(539569, 'George', 'Mardy', 27),
+(733843, 'Mario', 'Saule', 63),
+(631548, 'Alan', 'Snappy', 27),
+(839139, 'Maria', 'Foster', 57);
