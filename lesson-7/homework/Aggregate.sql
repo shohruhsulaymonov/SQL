07b518a363fc46cffc9acacbb170f2610aa8c6c9
Shohruh <shsulaymonov2007@gmail.com>
2025-04-05 19:55:36 +0500

    Update Aggregate.sql

--- lesson-7/homework/Aggregate.sql
+++ lesson-7/homework/Aggregate.sql
@@ -6,44 +6,41 @@ select max(salary) from employees
 --3. Write a query to count the number of rows in the Customers table using COUNT(*).
 select count(*) from customers
 --4. Write a query to count the number of unique product categories (COUNT(DISTINCT Category)) from the Products table.
-select count(distinct price) from products
---5. Write a query to find the total (SUM) sales for a particular product in the Sales table. 
-select ProductID , sum(saleamount) from sales group by ProductID
+select count(distinct Category) from products
+--5. Write a query to find the total (SUM) sales amount for the product with id 7 in the Sales table.
+select sum(SaleAmount) from sales where ProductID = 7
 --6. Write a query to calculate the average (AVG) age of employees in the Employees table.
 select avg(age) employees
 --7. Write a query that uses GROUP BY to count the number of employees in each department.
-select DepartmentName, count(EmployeeID) from EmployeeDepartments group by DepartmentName
+select DepartmentName, count(EmployeeID) from Employees group by DepartmentName
 --8. Write a query to show the minimum and maximum Price of products grouped by Category.
-select category, min(price) MinPrice, max(price) MaxPrice from Products2 group by category
---9. Write a query to calculate the total (SUM) sales per Region in the Sales table.
-select Region, sum(SaleAmount) from Sales group by Region
+select category, min(price) MinPrice, max(price) MaxPrice from Products group by category
+--9. Write a query to calculate the total (SUM) sales per Customer in the Sales table.
+select CustomerID, sum(SaleAmount) Total from sales group by CustomerID
 --10. Write a query to use HAVING to filter departments having more than 5 employees from the Employees table.
-select DepartmentName, count(EmployeeName) CountOfEmp from EmployeeDepartments group by DepartmentName having count(EmployeeName) > 5
-
-
+select DepartmentName, count(*) as CountOfEmp from Employees group by DepartmentName having count(*) > 5
 
 --● Medium-Level Tasks (10) 
---11. Write a query to calculate the total sales and average sales for each product category from the Sales table.
-select Category, sum(sales) Total, avg(sales) Average from Sales group by Category
+    --11. Write a query to calculate the total sales and average sales for each product category from the Sales table.
+    select Category, sum(sales) Total, avg(sales) Average from Sales group by Category
 --12. Write a query that uses COUNT(columnname) to count the number of employees with a specific JobTitle.
-select Departmentname, Count(Employeename) CountOfEmp from EmployeeDepartments group by Departmentname
+select Count(Employeename) CountOfEmp from Employees where Departmentname = 'HR'
 --13. Write a query that finds the highest (MAX) and lowest (MIN) Salary by department in the Employees table.
-select departmentid, max(salary) MaxSalary, min(salary) MinSalary from employees group by departmentid
+select DepartmentName, min(Salary) MinSalary, max(Salary) MaxSalary from Employees group by DepartmentName
 --14. Write a query that uses GROUP BY to calculate the average salary per Department.
-select departmentid, avg(salary) AvgSalary from employees group by departmentid
+select DepartmentName, avg(salary) AvgSalary from Employees group by DepartmentName
 --15. Write a query to show the AVG salary and COUNT(*) of employees working in each department.
-select departmentid, avg(salary) AvgSalary, count(*) CountOfEmp from employees group by departmentid
---16. Write a query that uses HAVING to filter products with an average price greater than 100.
-select Name, avg(price) as AvgPrice from Products group by Name having avg(price) > 100
---17. Write a query to count how many products have sales above 100 units using COUNT(DISTINCT ProductID).
-select COUNT(DISTINCT ProductID) from Sales where SaleAmount > 100
---18. Write a query that calculates the total sales for each year in the Sales table, and use GROUP BY to group them.
-select year(saledate) Year, count(year(saledate)) TotalSales from sales group by year(saledate)
+select DepartmentName, avg(salary) AvgSalary, count(*) CountOfEmp from Employees group by DepartmentName
+--16. Write a query that uses HAVING to filter product categories with an average price greater than 400.
+select Category, avg(Price) AvgPrice from Products group by Category having avg(Price) > 400
+--17. Write a query that calculates the total sales for each year in the Sales table, and use GROUP BY to group them.
+select year(SaleDate) Year, sum(Saleamount) from Sales group by year(SaleDate)
+--18. Write a query that uses COUNT to show the number of customers who placed at least 3 orders.
+select CustomerID, count(*) NumOfOrders from Orders group by CustomerID having count(*) >= 3
 --19. Write a query that uses COUNT to show the number of customers who placed orders in each region.
 select location, count(*) from customers group by location
 --20. Write a query that applies the HAVING clause to filter out Departments with total salary expenses greater than 100,000.
-select departmentid, sum(salary) from employees group by departmentid having sum(salary) > 100000
-
+select DepartmentName, sum(Salary) from Employees group by DepartmentName having not sum(Salary) > 500000
 
 
 --Hard-Level Tasks (10) 
